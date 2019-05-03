ee767ced72285ed812cf31e47ca8e1d497edd14b
callyho <callie.holmes@gmail.com>
2019-05-03 09:15:22 -0400

    Update package.py to split tar+zip
    Separating tar+zip from the package.py script so that this can be run separately from bagging. Renamed to bag.py

--- scripts/bag.py
+++ scripts/bag.py
@@ -1,5 +1,5 @@
-# Purpose: bag, tar, and zip each aip folder.
-# Dependencies: bagit.py, prepare_bag perl script
+# Purpose: bag and validate each aip folder.
+# Dependencies: bagit.py
 
 import os
 import shutil
@@ -32,17 +32,3 @@ for item in os.listdir():
   validation_result = subprocess.run(f'bagit.py --validate --quiet {item}_bag', stderr=subprocess.PIPE, shell=True)
   if 'invalid' in str(validation_result):
     move_error('bag_invalid', f'{item}_bag')
-  
-
-# Recalculating total in case any aips were invalid and moved.
-total = len(os.listdir(aips_directory))
-tarzip_count = 0
-
-# Tar and zip the aips using a Perl script.
-# Separate loop so won't get an error if any files are moved due to errors.
-for item in os.listdir():
-  # Displays a progress count because this step can take a long time.
-  tarzip_count += 1
-  print(f'Tar/zipping AIP {tarzip_count} of {total}.')
-
-  subprocess.run(f'perl {prepare_bag} {item} {aip_staging}/aips-ready-to-ingest/', shell=True)
