fc791a292723bada11e0d5325b13fb7443203969
callyho <callie.holmes@gmail.com>
2019-05-14 09:38:56 -0400

    Updated tar_zip.py to add an if loop
    Tests to see if the workflow is MKV. If so, runs the version of the Perl script that only tars (no zipping) the bags. All other workflows run the prepare_bag script that tars and zips the aips.

--- scripts/tar_zip.py
+++ scripts/tar_zip.py
@@ -1,4 +1,4 @@
-# Purpose: if MKV, tar each aip folder. All others: tar and zip each aip folder.
+# Purpose: if MKV, tar ONLY each aip folder. All others: tar and zip each aip folder.
 # Dependencies: prepare_bag perl script, prepare_bag_nozip perl script
 
 import os
@@ -10,11 +10,21 @@ from variables import *
 total = len(os.listdir(aips_directory))
 tarzip_count = 0
 
-# Tar and zip the aips using a Perl script.
-# Separate loop so won't get an error if any files are moved due to errors.
-for item in os.listdir():
-  # Displays a progress count because this step can take a long time.
-  tarzip_count += 1
-  print(f'Tar/zipping AIP {tarzip_count} of {total}.')
 
-  subprocess.run(f'perl {prepare_bag} {item} {aip_staging}/aips-ready-to-ingest/', shell=True)
+if workflow == 'mkv':
+  # Tar the aips using a Perl script.
+  # Separate loop so won't get an error if any files are moved due to errors.
+  for item in os.listdir():
+  # Displays a progress count because this step can take a long time.
+    tarzip_count += 1
+    print(f'Tar/zipping AIP {tarzip_count} of {total}.')
+    subprocess.run(f'perl {prepare_bag_nozip} {item} {aip_staging}/aips-ready-to-ingest/', shell=True)
+      
+else:
+  # Tar and zip the aips using a Perl script.
+  # Separate loop so won't get an error if any files are moved due to errors.
+  for item in os.listdir():
+  # Displays a progress count because this step can take a long time.
+    tarzip_count += 1
+    print(f'Tar/zipping AIP {tarzip_count} of {total}.')
+    subprocess.run(f'perl {prepare_bag} {item} {aip_staging}/aips-ready-to-ingest/', shell=True)
