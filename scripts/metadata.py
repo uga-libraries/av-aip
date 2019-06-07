fac7966b5818e1fdb0f4bcccb599d8cf54ae37a9
callyho <callie.holmes@gmail.com>
2019-06-07 15:15:33 -0400

    Update metadata.py - correcting filepath issue
    PBCore version of mediainfo wasn't being created correctly due to file path problem.

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -18,7 +18,7 @@ for item in os.listdir():
   shutil.copy2(f'{item}/metadata/{item}_mediainfo.xml', f'{aip_staging}/mediainfo-xmls')
 
   # Makes pbcore xml and saves to local server.  
-  subprocess.run(f'mediainfo -f --Output=PBCore2 --Language=raw {item}/objects/{item} > {aip_staging}/mediainfo-xmls/pbcore2-xml/{item}_mediainfo-pbcore2.xml', shell=True)
+  subprocess.run(f'mediainfo -f --Output=PBCore2 --Language=raw {item}/objects > {aip_staging}/mediainfo-xmls/pbcore2-xml/{item}_mediainfo-pbcore2.xml', shell=True)
      
   # For mxf, makes framemd5 and saves to metadata folder.
   if workflow == 'mxf':
