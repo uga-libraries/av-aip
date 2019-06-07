0a86e0ca5f69ae86e73c98bda6dc55f17f6e63f3
callyho <callie.holmes@gmail.com>
2019-06-07 15:48:25 -0400

    Update verify_fixity.py - fixed filepath
    Corrected syntax of the {fullpath} with checksumthing

--- scripts/verify_fixity.py
+++ scripts/verify_fixity.py
@@ -10,8 +10,7 @@ print('\n>>>Verifying fixity...\n')
 
 # Makes a manifest from the existing sidecar files.
 # NOT TESTED: couldn't get checksumthing configured right on my machine (AH)
-subprocess.run(f'checksumthing -i "{aips_directory}" -ie .md5 -t md5 -c lower -r -post "fullpath" -o "{aips_directory}"/oldmd5manifest.txt', shell=True)
-
+subprocess.run(f'checksumthing -i "{aips_directory}" -ie .md5 -t md5 -c lower -r -post \' {{fullpath}}\' -o "{aips_directory}"/oldmd5manifest.txt', shell=True)
 for item in os.listdir():
 
   #Skips the manifest, which are produced by the previous step, and temporary files (their fixity often changes).
