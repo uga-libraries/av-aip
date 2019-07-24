c41ed4ca582d77a5bfa86c400ff5eefd4d45afa5
callyho <callie.holmes@gmail.com>
2019-07-24 09:58:55 -0400

    Added workflow type
    Added mkv-filmscan workflow type

--- scripts/duplicate_check.py
+++ scripts/duplicate_check.py
@@ -18,7 +18,7 @@ for item in os.listdir():
     else:
       aip_id = f'bmac_{item}'
 	
-  if workflow == 'mkv' or workflow == 'mov':
+  if workflow == 'mkv' or workflow == 'mov' or workflow == 'mkv-filmscan':
     aip_id = f'bmac_{pathlib.Path(item).stem}'
   
   if workflow == 'mxf':
