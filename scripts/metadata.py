716125e68657ca6c8cbca90b537d8074ca0b1ffb
callyho <callie.holmes@gmail.com>
2020-05-17 20:13:50 -0400

    Changing directory for copied files
    Changing the location where the raw mediainfo xml output is copied to -- is now a subfolder under mediainfo-xmls

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -15,7 +15,7 @@ for item in os.listdir():
   subprocess.run(f'mediainfo -f --Output=XML --Language=raw {item}/objects > {item}/metadata/{item}_mediainfo.xml', shell=True)
     
   # Copies mediainfo XML to local server for staff use.  
-  shutil.copy2(f'{item}/metadata/{item}_mediainfo.xml', f'{aip_staging}/mediainfo-xmls')
+  shutil.copy2(f'{item}/metadata/{item}_mediainfo.xml', f'{aip_staging}/mediainfo-xmls/mediainfo-raw-output')
 
   # Makes pbcore xml and saves to local server.  
   subprocess.run(f'mediainfo -f --Output=PBCore2 --Language=raw {item}/objects > {aip_staging}/mediainfo-xmls/pbcore2-xml/{item}_mediainfo-pbcore2.xml', shell=True)
